29baa865af6abf2240ebfd1658a2595199bbc44f
Barış Özmen <hbaristr@gmail.com>
2024-12-06 21:12:16 +0300

    new metaclasses

--- metaclasses.py
+++ metaclasses.py
@@ -24,3 +24,24 @@ class DebuggerOnErrorMetaclass(type):
             if callable(value) and not key.startswith('_'):
                 attrs[key] = debugger_on_error(value)
         return super().__new__(cls, name, bases, attrs)
+
+
+
+
+class LoggedMetaclass(type):
+    """Metaclass that logs all method calls for a class"""
+    def __new__(cls, name, bases, attrs):
+        # Wrap callable methods with logging
+        for key, value in attrs.items():
+            if callable(value) and not key.startswith('_'):
+                attrs[key] = cls.log_call(value)
+        return super().__new__(cls, name, bases, attrs)
+    
+    @staticmethod
+    def log_call(func):
+        def wrapper(*args, **kwargs):
+            print(f'Calling: {func.__name__}')
+            result = func(*args, **kwargs)
+            print(f'Finished: {func.__name__}')
+            return result
+        return wrapper
