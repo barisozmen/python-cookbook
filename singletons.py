9a6cf351a3cdb5e7f199e8c90601f76cba30e144
Barış Özmen <hbaristr@gmail.com>
2024-12-06 21:21:15 +0300

    config singleton added

--- singletons.py
+++ singletons.py
@@ -8,3 +8,14 @@ class Counter(metaclass=SingletonMetaclass):
     def next_id(self):
         self.count += 1
         return self.count
+    
+
+class Config(metaclass=SingletonMetaclass):
+    def __init__(self):
+        self._settings = {}
+    
+    def set(self, key, value):
+        self._settings[key] = value
+        
+    def get(self, key, default=None):
+        return self._settings.get(key, default)
