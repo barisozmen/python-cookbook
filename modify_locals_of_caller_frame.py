ecf562fd63798805a9cb0096b40407cfddf03867
Barış Özmen <hbaristr@gmail.com>
2025-03-08 03:17:00 +0300

    Update and rename dangerous.py to modify_locals_of_caller_frame.py

--- modify_locals_of_caller_frame.py
+++ modify_locals_of_caller_frame.py
@@ -1,16 +1,17 @@
 import ctypes
 import inspect
 
-def modify_locals():
+def modify_local_of_caller_frame():
     frame = inspect.currentframe()
+    
     # Modify the locals dictionary
     frame.f_locals['x'] = 42
+    
     # Sync the change to fast locals
     ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(frame), ctypes.c_int(0))
-    print(x)  # Now x is 42 in this scope!
 
 
 x=5
 print(x, '<--before')
-modify_locals()
+modify_local_of_caller_frame()
 print(x, '<--after')
